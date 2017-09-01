2cb6a187c8391a4e8410f8e44a88618c436e85ba
alecmac <alecmac@users.noreply.github.com>
2017-08-31 20:15:58 -0700

    fixed repo request again (actually get repos)

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -12,7 +12,7 @@ var app = app || {};
     //       client side of our app, our new proxyGitHub function will be handling the token using our
     //       new environment variable!
 
-    $.get('/github/users/codefellows').then(data => repos.all = data, err => console.error(err))
+    $.get('/github/users/codefellows/repos').then(data => repos.all = data, err => console.error(err))
     .then(callback);
   };
 
